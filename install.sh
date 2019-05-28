4c29b6e3bcb057a3e99cd7df89836f14e7059de3
Riley Carpenter <rileyballcarpenter@gmail.com>
2019-05-28 10:15:17 -0400

    auto download requirements

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ if [ "$EUID" -ne 0 ]
 fi
 chmod +x installfile/devito
 cp installfile/devito /usr/bin/
+pip3 install -r requirements.txt
 cp installfile/main.py installfile/runDevito.py
 mv installfile/runDevito.py /usr/bin/
